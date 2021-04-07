99c2b5ef3c027260fc536ec22b89d0724a640486
Raphael Ndauwa <raphaelndauwa@gmail.com>
2021-04-07 23:27:27 +0300

    add redux files

---
+++ src/redux/constants/api/auth.ts
@@ -0,0 +1,11 @@
+
+import axios, { AxiosRequestConfig } from 'axios';
+import {url} from "./config";
+
+export const loginUserApi = (user: any, config: AxiosRequestConfig | undefined) => {
+    return axios.post(url +'/api/v1/oauth/token/', user, config)
+};
+
+export const logoutUserApi = (config: AxiosRequestConfig | undefined) => {
+    return axios.post(url +'api/auth/logout', null, config);
+};
