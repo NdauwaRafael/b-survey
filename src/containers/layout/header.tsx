f02c82dabf9085459e39e704360a57d33e39900d
Raphael Ndauwa <raphaelndauwa@gmail.com>
2021-04-08 19:07:34 +0300

    customize links and include dashboard, surveys and profile

--- src/containers/layout/header.tsx
+++ src/containers/layout/header.tsx
@@ -32,12 +32,15 @@ const Header = ({isAuthenticated}: any) => (
                 {
                     isAuthenticated ?
                          <nav className="hidden md:flex space-x-10">
-                            <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
-                                Pricing
-                            </a>
-                            <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
-                                Docs
-                            </a>
+                            <Link to="/" className="text-base font-medium text-gray-500 hover:text-gray-900">
+                                Dashboard
+                            </Link>
+                            <Link to="/surveys" className="text-base font-medium text-gray-500 hover:text-gray-900">
+                                Surveys
+                            </Link>
+                            <Link to="/profile" className="text-base font-medium text-gray-500 hover:text-gray-900">
+                                Profile
+                            </Link>
                         </nav>
                         :
                         <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
