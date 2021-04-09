3d3929340312d576885611adb15f4f4e8c80cf34
Raphael Ndauwa <raphaelndauwa@gmail.com>
2021-04-09 11:29:18 +0300

    survey components

--- src/containers/surveys/survey.tsx
+++ src/containers/surveys/survey.tsx
@@ -17,8 +17,24 @@ const Survey = ({match}: Props) => {
     }, [dispatch, forms]);
 
     return (
-        <div>
-            <h2>Survey Details</h2>
+        <div className="px-8 py-6">
+            <div className="flex flex-col">
+                <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+
+                        <div className="px-4 sm:px-0 mb-5">
+                            <h3 className="text-lg font-medium leading-6 text-gray-900">Survey Details</h3>
+                            <p className="mt-1 text-sm text-gray-600">
+                                Fill out the survey details below
+                            </p>
+                        </div>
+
+                        <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
