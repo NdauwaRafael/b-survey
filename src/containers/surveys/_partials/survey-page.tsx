0949bf530656075fa4d6c2dae6e598954c9a7986
Raphael Ndauwa <raphaelndauwa@gmail.com>
2021-04-09 16:24:21 +0300

    add stepped form and a campaign initialization step

--- src/containers/surveys/_partials/survey-page.tsx
+++ src/containers/surveys/_partials/survey-page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {SurveySection} from "./survey.section";
 
-const SurveyPage = ({page}: any) => {
+const SurveyPage = ({page, pageCount, changePage, totalPages, submit}: any) => {
     const {sections} = page;
     return (
         <div className="px-4 py-4 shadow overflow-hidden border-b border-gray-200 sm:rounded-lg mb-4">
@@ -9,12 +9,44 @@ const SurveyPage = ({page}: any) => {
                 <div dangerouslySetInnerHTML={{__html: page.description}} />
             </div>
 
-            <div >
+            <div className="mb-3">
                 {
                     sections ?
                         sections.map((section: any) => <SurveySection key={section.id} section={section} />)
                         :null
                 }
+            </div>
+
+            <div className="py-3 sm:px-6 flex items-center justify-between">
+                <div className="text-left">
+                    {
+                        pageCount > 0 ?
+                            <button type="button"
+                                    onClick={()=>changePage(-1)}
+                                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-indigo-600 bg-white hover:bg-grey-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-grey-300">
+                                Previous
+                            </button>
+                            : null
+                    }
+                </div>
+
+                <div className="text-right">
+                    {
+                        pageCount < totalPages ?
+                            <button type="button"
+                                    onClick={()=>changePage(+1)}
+                                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                Next
+                            </button>
+                            :
+                            <button type="button"
+                                    onClick={()=>submit()}
+                                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                Submit
+                            </button>
+                    }
+
+                </div>
             </div>
 
         </div>
--- src/containers/surveys/survey.tsx
+++ src/containers/surveys/survey.tsx
@@ -1,21 +1,49 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {Props} from "../../_helpers/_route-props";
 import {RootStateOrAny, useDispatch, useSelector} from "react-redux";
 import {getFormFromId} from "./_helpers/survey.helpers";
 import {loadSurveyForms} from "../../redux/actions/survey";
 import {SurveyPage} from "./_partials/survey-page";
 
+interface IStartData {
+    start: boolean;
+    startTime: any;
+}
+
 const Survey = ({match}: Props) => {
     const dispatch = useDispatch();
     const forms = useSelector((state: RootStateOrAny) => state.surveys.forms);
     const form = getFormFromId(forms, match.params.id);
     const {pages} = form;
+    const [pageCount, setPageCount] = useState(0);
+    const [startData, setStartData] = useState<IStartData>({start: false, startTime: undefined});
+    const currentPage = pages ? pages[pageCount] : {};
+
 
     useEffect(() => {
         if (!forms || forms.length === 0) {
             dispatch(loadSurveyForms());
         }
     }, [dispatch, forms]);
+
+    const handleChangePage = (count: number)=>{
+        if (pages){
+            if (pageCount < pages.length){
+                setPageCount(pageCount + count)
+            }
+        }
+    };
+
+    const handleStartSurvey = ()=>{
+        setStartData({
+            start: true,
+            startTime: Date.now()
+        })
+    };
+
+    const handleSubmit = ()=>{
+
+    };
 
     return (
         <div className="px-8 py-6">
@@ -31,12 +59,38 @@ const Survey = ({match}: Props) => {
                         </div>
 
                         <div className="">
-                            <form>
-                                {pages ?
-                                    pages.map((page: any) => <SurveyPage key={page.id} page={page}/>)
-                                    : null
-                                }
-                            </form>
+                            {
+                                startData.start ?
+                                    <form>
+                                        <SurveyPage
+                                            key={currentPage.id}
+                                            page={currentPage}
+                                            totalPages={pages ? pages.length - 1 : 0}
+                                            pageCount={pageCount}
+                                            submit={handleSubmit}
+                                            changePage={handleChangePage}/>
+                                    </form>
+                                    :
+                                    <div className="bg-gray-50">
+                                        <div
+                                            className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+                                            <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                                                <span className="block">Ready to dive in?</span>
+                                                <span
+                                                    className="block text-indigo-600">Start the campaign now.</span>
+                                            </h2>
+                                            <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+                                                <div className="inline-flex rounded-md shadow">
+                                                    <button onClick={()=>handleStartSurvey()}
+                                                       className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
+                                                        Get started
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                            }
                         </div>
                     </div>
                 </div>
